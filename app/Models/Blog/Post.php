3a349a6bd62b75ae936f5f552be35212775662d6
andreslqr <me@andreslopez.com.mx>
2024-01-18 23:08:03 -0600

    model definition added

--- app/Models/Blog/Post.php
+++ app/Models/Blog/Post.php
@@ -4,8 +4,48 @@ namespace App\Models\Blog;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'title',
+        'short_description',
+        'content',
+        'author',
+        'published_at',
+        'expired_at',
+        'is_active'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_active' => 'boolean',
+        'content' => 'array',
+        'published_at' => 'datetime',
+        'expired_at' => 'datetime' 
+    ];
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'is_active' => false,
+        'content' => '[]',
+    ];
 }
--- database/migrations/2024_01_18_223620_create_posts_table.php
+++ database/migrations/2024_01_18_223620_create_posts_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
+            $table->string('title');
+            $table->string('short_description', 510);
+            $table->json('content');
+            $table->string('author')->nullable();
+            $table->timestamp('published_at');
+            $table->timestamp('expired_at')->nullable();
+            $table->boolean('is_active')->default(false);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
