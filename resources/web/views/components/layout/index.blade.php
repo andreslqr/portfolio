0cf344971cf23a765ca3b70e36ad20ec16e681c5
andreslqr <me@andreslopez.com.mx>
2024-01-31 21:46:23 -0600

    added favicon

--- resources/web/views/components/layout/index.blade.php
+++ resources/web/views/components/layout/index.blade.php
@@ -9,6 +9,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ Vite::asset('resources/web/meta/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ Vite::asset('resources/web/meta/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ Vite::asset('resources/web/meta/' . app()->getLocale() . '-site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ Vite::asset('resources/web/meta/safari-pinned-tab.svg') }}" color="#2557e0">
+    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="theme-color" content="#ffffff">
+
+    {{ $meta ?? null }}
     @livewireStyles
     @vite('resources/web/sass/app.scss')
     @stack('styles')
