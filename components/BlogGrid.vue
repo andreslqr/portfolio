21f588f1b73170e1d444f532890be48bdd1c08a9
andreslqr <me@andreslopez.com.mx>
2025-07-15 13:43:09 -0600

    new entry about early return

--- components/BlogGrid.vue
+++ components/BlogGrid.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-3">
     <slot />
   </div>
 </template>
